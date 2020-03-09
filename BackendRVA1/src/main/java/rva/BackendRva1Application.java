3f234ae73a4a335187d2f3b40501d461c4da1d33
it18-2016 <it18-2016@AR1-ASISTENT.mshome.net>
2020-03-09 17:20:00 +0100

    initial commit

---
+++ BackendRVA1/src/main/java/rva/BackendRva1Application.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRva1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRva1Application.class, args);
+	}
+
+}
---
+++ BackendRVA1/src/test/java/rva/BackendRva1ApplicationTests.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRva1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
